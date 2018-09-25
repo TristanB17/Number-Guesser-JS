c2afc08a73f339bdb8dfac54e20a2bbd5281126c
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-09-25 15:15:51 -0600

    got visibility working

--- number_generator_and_guess_checker.js
+++ number_generator_and_guess_checker.js
@@ -1,6 +1,7 @@
 let baseNumber = 1;
 let limitNumber = 100;
 var randomNumber = Math.floor(Math.random() * (baseNumber - limitNumber + 1)) + baseNumber;
+document.getElementById('check-guess').style.visibility = "hidden"
 
 function resetPage() {
   location.reload()
@@ -8,8 +9,9 @@ function resetPage() {
 
 function apparate() {
   x = document.getElementById('check-guess');
+  event.preventDefault();
   console.log('infants')
-  debugger;
+  console.log(x.style.visibility)
   if (x.style.visibility === "hidden") {
     console.log('babies')
     x.style.visibility = "visible";
